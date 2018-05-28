3a7eea81f4971ad05a681d983514f3e7b2cf655e
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-28 11:50:33 +0200

    - move styles to separate file

---
+++ XamarinEssentialsApp/XamarinEssentialsApp/XamarinEssentialsApp/Styles/Styles.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinEssentialsApp.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Styles : ResourceDictionary
+	{
+		public Styles ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
