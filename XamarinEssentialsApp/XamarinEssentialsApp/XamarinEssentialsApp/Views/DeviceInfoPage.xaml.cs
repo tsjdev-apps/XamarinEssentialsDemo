94154f584fad013f58c190429e7ae07b4d267fc4
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-28 12:48:23 +0200

    - add deviceinfopage

---
+++ XamarinEssentialsApp/XamarinEssentialsApp/XamarinEssentialsApp/Views/DeviceInfoPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinEssentialsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeviceInfoPage : ContentPage
+    {
+        public DeviceInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            DeviceLabel.Text = DeviceInfo.Model;
+            ManufacturerLabel.Text = DeviceInfo.Manufacturer;
+            DeviceNameLabel.Text = DeviceInfo.Name;
+            VersionLabel.Text = DeviceInfo.VersionString;
+            PlatformLabel.Text = DeviceInfo.Platform;
+            IdiomLabel.Text = DeviceInfo.Idiom;
+            DeviceTypeLabel.Text = DeviceInfo.DeviceType.ToString();
+        }
+    }
+}
