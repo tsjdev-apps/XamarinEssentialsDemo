677d8d54c9a4eff963e2b09bf797b39adac57ab4
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-28 11:44:36 +0200

    - add batterypage

---
+++ XamarinEssentialsApp/XamarinEssentialsApp/XamarinEssentialsApp/Views/BatteryPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinEssentialsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BatteryPage : ContentPage
+	{
+		public BatteryPage ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BatteryLevelLabel.Text = Battery.ChargeLevel.ToString();
+            BatteryStateLabel.Text = Battery.State.ToString();
+            BatteryPowerSourceLabel.Text = Battery.PowerSource.ToString();
+        }
+    }
+}
