40ab3198a0752acbf811a78c1876bb20be053d54
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-28 12:00:51 +0200

    - add devicedisplaypage

---
+++ XamarinEssentialsApp/XamarinEssentialsApp/XamarinEssentialsApp/Views/DeviceDisplayPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinEssentialsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeviceDisplayPage : ContentPage
+    {
+        public DeviceDisplayPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var metrics = DeviceDisplay.ScreenMetrics;
+
+            OrientationLabel.Text = metrics.Orientation.ToString();
+            RotationLabel.Text = metrics.Rotation.ToString();
+            ScreenWidthLabel.Text = metrics.Width.ToString();
+            ScreenHeightLabel.Text = metrics.Height.ToString();
+            DensityLabel.Text = metrics.Density.ToString();
+        }
+    }
+}
